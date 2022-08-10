742be295abb7a018f9ab50884a3027073631c7fa
codewithnamo <68044216+codewithnamo@users.noreply.github.com>
2022-08-10 04:33:01 -0400

    Update Cards.js

--- components/Cards.js
+++ components/Cards.js
@@ -94,37 +94,35 @@ const Cards = () => {
       <div className={styles.cards}>
         {events &&
           events?.map((event) => (
-            <div>
-              {event && (
-                <Link key={event._id} href={`/events/${event.slug.current}`}>
-                  <div className="flex h-full flex-1 cursor-pointer flex-col justify-between overflow-hidden rounded-md shadow-lg transition-transform duration-200 ease-in-out hover:scale-105">
-                    <div>
-                      {" "}
-                      {event.mainImage && (
-                        <img
-                          className="h-60 w-full object-cover "
-                          src={urlFor(event.mainImage).url()}
-                          alt=""
-                        />
-                      )}
-                      <div className="flex items-center bg-white px-3 pt-3 md:px-5 md:pt-5">
-                        <div className="flex h-full w-full flex-1 flex-col ">
-                          <div className=" ">
-                            <p className="text-sm text-darkteal">
-                              {showEventDate(event.startTime, event.endTime)}
-                            </p>
-                            <p className="text-xl font-semibold text-darkblue md:text-lg">
-                              {event?.title}
-                            </p>
-                            <p className="text-sm leading-6 text-black">
-                              {console.log(event.summary)}
-                              {event?.summary}
-                            </p>
-                            <p className="text-sm leading-6 text-black">
-                              {console.log(event.transferDate)}
-                              {event?.transferDate}
-                            </p>
-                          </div>
+            <Link key={event._id} href={`/events/${event.slug.current}`}>
+              <div className="flex h-full flex-1 cursor-pointer flex-col justify-between overflow-hidden rounded-md shadow-lg transition-transform duration-200 ease-in-out hover:scale-105">
+                {event && (
+                  <div>
+                    {" "}
+                    {event.mainImage && (
+                      <img
+                        className="h-60 w-full object-cover "
+                        src={urlFor(event.mainImage).url()}
+                        alt=""
+                      />
+                    )}
+                    <div className="flex items-center bg-white px-3 pt-3 md:px-5 md:pt-5">
+                      <div className="flex h-full w-full flex-1 flex-col ">
+                        <div className=" ">
+                          <p className="text-sm text-darkteal">
+                            {showEventDate(event.startTime, event.endTime)}
+                          </p>
+                          <p className="text-xl font-semibold text-darkblue md:text-lg">
+                            {event?.title}
+                          </p>
+                          <p className="text-sm leading-6 text-black">
+                            {console.log(event.summary)}
+                            {event?.summary}
+                          </p>
+                          <p className="text-sm leading-6 text-black">
+                            {console.log(event.transferDate)}
+                            {event?.transferDate}
+                          </p>
                         </div>
                       </div>
                     </div>
@@ -138,9 +136,9 @@ const Cards = () => {
                       </div>
                     </div>
                   </div>
-                </Link>
-              )}
-            </div>
+                )}
+              </div>
+            </Link>
           ))}
       </div>
     </div>
